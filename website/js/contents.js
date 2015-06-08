89f8126bd32e7eae9cdfa12818e958158b713ee0
Ben Gamari <bgamari.foss@gmail.com>
2015-06-08 16:30:55 -0400

    contents: Lowercase query string

--- website/js/contents.js
+++ website/js/contents.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
     $('#package-filter').keyup(function() {
-        var query = $(this).val();
+        var query = $(this).val().toLowerCase();
         if (query == '') {
             $('.history tbody tr').show();
         } else {
             $(".history tbody td:first-child").filter(function() {
-                return $(this).text().indexOf(query) == -1;
+                return $(this).text().toLowerCase().indexOf(query) == -1;
             }).parent().hide();
         }
     });
